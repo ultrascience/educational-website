5f36a6232f100e0072ba3e62b885a88f795f412f
Aldo Rene Hernandez Zacateco <aldorene@ciencias.unam.mx>
2021-10-05 23:02:44 +0000

    ADDED: Componente boton agregado Nota: Es un modelo de boton sencillo

--- sitio-web/src/App.tsx
+++ sitio-web/src/App.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import BoxModel from './components/BoxModel'
+import Button from './components/Button'
 
 function App() {
   return (
-    <BoxModel />
+  <>
+  <div className="container mx-auto px-4 h-screen">
+  <BoxModel /> 
+  <Button />
+  </div>
+  </>
   );
 }
 
-export default App;
+export default App
--- sitio-web/src/components/Button.tsx
+++ sitio-web/src/components/Button.tsx
@@ -1,6 +1,7 @@
+function Button() {
+  return (
+  <button className="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded">Boton</button>
+  );
 
-import React from 'react';
-
-function Button() {}
-
+}
 export default Button;
