080df4c70e72804dc0c789a1a092228391fc9352
Aldo Rene Hernandez Zacateco <aldorene@ciencias.unam.mx>
2021-10-18 22:30:20 +0000

    ADDED: Function que muestra la ventana de informacion cuando se hace click en el boton informacion.

--- sitio-web/src/components/Scene3D.tsx
+++ sitio-web/src/components/Scene3D.tsx
@@ -6,17 +6,33 @@ import Button from './Button'
 import Modelo3D from "./Modelo3D";
 import Augen from "./Augen";
 import "../index.css"
+import ReactDOM from 'react-dom';
+import React from "react";
+
+
+function showInformation(){
+ const container = document.getElementById("container");
+ const element = React.createElement('div',{className: 'containerInformation'},'Hola');
+ ReactDOM.render(element,container);
+}
+
 function Scene3D() {
  return (
   <div className="grid-scene">
-  <div className="grid-model">
+  <div id="container" className="grid-model">
    <Modelo3D modelo={<Augen />} />
   </div>
   <div className="grid-buttons">
   <div className="flex gap-8">
-      <Button title="Propiedades fisicas"/>
-      <Button title="Propiedades quimicas"/>
-      <Button title="Propiedades biologicas"/>
+      {/*<Button title="Propiedades fisicas"/>*/}
+      {/*<Button title="Propiedades quimicas"/>*/}
+      {/*<Button title="Propiedades biologicas"/>*/}
+      {/*<Button title="Propiedades Cristalograficas"/>*/}
+      {/*<Button title="Propiedades Opticas"/>*/}
+   <Button
+       onClick={showInformation}
+       children = "Informacion"
+   />
   </div>
   </div>
   </div>
