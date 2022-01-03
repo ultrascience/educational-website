5eabe6f7f4525a5df6e3881fddbba56c3fb19bec
Aldo Rene Hernandez Zacateco <aldorene@ciencias.unam.mx>
2022-01-03 19:18:50 +0000

    ADDED: inputs form generate

--- client/src/components/Forms.tsx
+++ client/src/components/Forms.tsx
@@ -8,7 +8,7 @@ function Forms() {
 
   // Define type to uploads images
   // type File = Blob | File;
-  const namesArray = ["name", "description", "image"];
+  const namesArray = ["name", "description", "image", "classification"];
   const [error, setError] = useState(false);
   const [success, setSuccess] = useState(false);
   const [loading, setLoading] = useState(false);
@@ -63,15 +63,21 @@ function Forms() {
   const generateInputs = () => {
     return namesArray.map((name, index) => {
       return (
-        <div className="flex flex-col mb-4" key={index}>
+        <div className="mb-4" key={index}>
           <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor={name}>{name}</label>
-          <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id={name} type="text" placeholder={name} />
+          <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id={name} type="text" placeholder={name} onChange={handleChange} />
         </div>
       );
     });
   };
+  //
+  // handleChange function to update jsonPrueba with the values of the inputs
+   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => { 
+    const { name, value } = event.target;
+    setJsonPrueba({ ...jsonPrueba, [name]: value }); 
+    }; 
 
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+  const handleSubmit = (e: any) => {
     e.preventDefault();
     setLoading(true);
     axios.post("http://localhost:8080/api/rocks/", jsonPrueba)
@@ -94,6 +100,7 @@ function Forms() {
       <div className="flex flex-wrap justify-center">
         <div className="focus-within:border-transparent border-2 border-gray-300 rounded-lg p-4 w-full md:w-1/2">
           <form onSubmit={handleSubmit}>
+            {generateInputs()}
             <div className="mb-4">
               <label
                 className="block text-gray-700 text-sm font-bold mb-2"
@@ -105,13 +112,16 @@ function Forms() {
             </div>
           </form>
 
-          <button
-            className={`${loading ? "cursor-not-allowed opacity-50" : "cursor-pointer"} bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline`}
-            type="submit"
-            onClick={handleSubmit}
-          >
-            {loading ? "Loading" : "Send"}
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit" onClick={handleSubmit}>
+            {loading ? "Loading..." : "Submit"}
           </button>
+          {error && <p className="text-red-500 text-xs italic">Error</p>}
+          {success && <p className="text-green-500 text-xs italic">Success</p>}
+          {fileUpload && <p>{fileUpload.name}</p>}
+          {fileUpload && <img src={URL.createObjectURL(fileUpload)} alt="uploaded" />}
+          {fileUpload && <p>{fileUpload.type}</p>}
+          {fileUpload && <p>{fileUpload.size}</p>}
+
         </div>
 
       </div>
