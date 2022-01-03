fd068e68301c69a3c23727e59a813ad5fbefc531
aldohernandezzacateco <zac@ciencias.unam.mx>
2022-01-03 12:56:43 -0600

    ADDED: generate inputs form

--- client/src/components/Forms.tsx
+++ client/src/components/Forms.tsx
@@ -58,28 +58,35 @@ function Forms() {
     references: "",
     // });
   });
-
-  const handleSubmit = async (e: { preventDefault: () => void }) => {
-    e.preventDefault();
-    setLoading(true);
-    try {
-      const response = await axios.post(
-        "http://localhost:8080/api/rocks/",
-        jsonPrueba
+  //
+  // Function to generate inputs for the form from namesArray with tailwindcss and typescript
+  const generateInputs = () => {
+    return namesArray.map((name, index) => {
+      return (
+        <div className="flex flex-col mb-4" key={index}>
+          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor={name}>{name}</label>
+          <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id={name} type="text" placeholder={name} />
+        </div>
       );
-      if (response.status === 201) {
-        setSuccess(true);
-        setLoading(false);
-      }
-    } catch (error) {
-      setError(true);
-      setLoading(false);
-    }
+    });
   };
 
-  const handleChange = (e: { target: { name: any; value: any } }) => {
-    const { name, value } = e.target;
-    setJsonPrueba({ ...jsonPrueba, [name]: value });
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setLoading(true);
+    axios.post("http://localhost:8080/api/rocks/", jsonPrueba)
+      .then(() => {
+        setLoading(false);
+        setSuccess(true);
+      })
+      .catch(() => {
+        setLoading(false);
+        setError(true);
+      });
+  };
+
+  const handleFileUpload = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFileUpload(e.target.files?.[0]);
   };
 
   return (
@@ -87,23 +94,6 @@ function Forms() {
       <div className="flex flex-wrap justify-center">
         <div className="focus-within:border-transparent border-2 border-gray-300 rounded-lg p-4 w-full md:w-1/2">
           <form onSubmit={handleSubmit}>
-          namesArray.map((name) => {
-            return (
-            <div className="mb-4">
-              <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor={name}>
-                {name}
-              </label>
-              <input
-                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                id={name}
-                type="text"
-                name={name}
-                value={jsonPrueba[name]}
-                onChange={handleChange}
-              />
-            </div>
-            );
-            });
             <div className="mb-4">
               <label
                 className="block text-gray-700 text-sm font-bold mb-2"
