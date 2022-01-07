e9401a8664665bb1dba8ccbb62828d1e60d58d95
Aldo Rene Hernandez Zacateco <aldorene@ciencias.unam.mx>
2022-01-07 21:13:22 +0000

    ADDED: Form without objects

--- client/src/components/Forms.tsx
+++ client/src/components/Forms.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { useState } from "react";
 import { JsonProps } from "./Types";
-import '../styles/button.css';
+import '../styles/index.css';
 
 // Component that send jsonPrueba to the server route localhost:8080/api/rocks/ with tailwindcss and typescript
 function Forms() {
@@ -15,46 +15,46 @@ function Forms() {
   //
   const [jsonPrueba, setJsonPrueba] = useState<JsonProps>(
     {
-      name: "joselin",
-      image: "joselin",
-      clasification: "joselin",
+      name: "",
+      image: "",
+      clasification: "",
       introduction: {
-        etymology: "joselin",
-        atmosphere: "joselin",
-        applications: "joselin",
-        main_locations: "joselin",
-        diffractogram: "joselin",
+        etymology: "",
+        atmosphere: "",
+        applications: "",
+        main_locations: "",
+        diffractogram: "",
       },
       properties: {
         chemical: {
-          chemical_formula: "joselin",
-          molecular_weight: "joselin",
-          elemental_chemistry: "joselin",
-          chemistry_oxides: "joselin",
+          chemical_formula: "",
+          molecular_weight: "",
+          elemental_chemistry: "",
+          chemistry_oxides: "",
         },
         crystallographic:
         {
-          cell_dimension: "joselin",
-          crystalline_system: "joselin",
-          x_ray_diffraction: "joselin",
+          cell_dimension: "",
+          crystalline_system: "",
+          x_ray_diffraction: "",
         }
         ,
         physical: {
-          gloss: "joselin",
-          color: "joselin",
-          hardness: "joselin",
-          stripe: "joselin",
-          fracture: "joselin",
-          crystal_habit: "joselin",
-          diaphanous: "joselin",
-          exfoliation: "joselin",
-          density: "joselin",
-          luminescence: "joselin",
-          radioactivity: "joselin",
+          gloss: "",
+          color: "",
+          hardness: "",
+          stripe: "",
+          fracture: "",
+          crystal_habit: "",
+          diaphanous: "",
+          exfoliation: "",
+          density: "",
+          luminescence: "",
+          radioactivity: "",
         },
-        optical: "joselin",
+        optical: "",
       },
-      references: "joselin",
+      references: "",
       // });
     }
   );
@@ -82,15 +82,38 @@ function Forms() {
       <div className="flex flex-wrap justify-center">
         <div className="focus-within:border-transparent border-2 border-gray-300 rounded-lg p-4 w-full md:w-1/2">
           <form onSubmit={handleSubmit}>
+
             <div className="mb-4">
               <label
-                className="block text-gray-700 text-sm font-bold mb-2"
-                htmlFor="clasification"
+                className="form-label"
+                htmlFor="name"
               >
-                Clasification
+                Nombre del Modelo
               </label>
               <input
-                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                className="form-input"
+                id="name"
+                type="text"
+                name="name"
+                placeholder="Enter the name of the model"
+                onChange={(event: any) => {
+                  setJsonPrueba({
+                    ...jsonPrueba,
+                    name: event.target.value,
+                  });
+                }}
+              />
+            </div>
+
+            <div className="mb-4">
+              <label
+                className="form-label"
+                htmlFor="clasification"
+              >
+                Clasificación
+              </label>
+              <input
+                className="form-input"
                 id="clasification"
                 type="text"
                 name="clasification"
@@ -101,39 +124,89 @@ function Forms() {
                     clasification: event.target.value
                   });
                 }}
-                value={jsonPrueba.clasification}
               />
             </div>
+
             <div className="mb-4">
-            <label 
-              className="block text-gray-700 text-sm font-bold mb-2" 
-              htmlFor="introduction"
-            >
-              Introduction
-            </label>
-            <input 
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" 
-              id="introduction" 
-              type="text" 
-              name="introduction" 
-              placeholder="Enter the introduction"
-              onChange={(event: any) => {
-                setJsonPrueba({
-                  ...jsonPrueba,
-                  // introduction: event.target.value
-                  introduction: {
-                    ...jsonPrueba.introduction,
-                    etymology: event.target.value,
-                    atmosphere: event.target.value
-                  }
-                });
-              }}
-              value={"Valor de prueba"}
-            />
+              <label
+                className="form-label"
+                htmlFor="introduction"
+              >
+                Introduction
+              </label>
+              <input
+                className="form-input"
+                id="introduction"
+                type="text"
+                name="introduction"
+                placeholder="Enter the introduction"
+                onChange={(event: any) => {
+                  setJsonPrueba({
+                    ...jsonPrueba,
+                    introduction: {
+                      ...jsonPrueba.introduction,
+                      etymology: event.target.value,
+                    }
+                  });
+                }}
+              />
             </div>
+
+            <div className="mb-4">
+              <label
+                className="form-label"
+                htmlFor="Propiedades"
+              >
+                Propiedades
+              </label>
+              <input
+                className="form-input"
+                id="Propiedades"
+                type="text"
+                name="Propiedades"
+                placeholder="Enter the properties"
+                onChange={(event: any) => {
+                  setJsonPrueba({
+                    ...jsonPrueba,
+                    properties: {
+                      ...jsonPrueba.properties,
+                      chemical: {
+                        ...jsonPrueba.properties.chemical,
+                        chemical_formula: event.target.value,
+                      }
+                    }
+                  });
+                }}
+              />
+            </div>
+
+            <div className="mb-4">
+              <label
+                className="form-label"
+                htmlFor="Referencias"
+              >
+                Referencias
+              </label>
+              <input
+                className="form-input"
+                id="Referencias"
+                type="text"
+                name="Referencias"
+                placeholder="Enter the references"
+                onChange={(event: any) => {
+                  setJsonPrueba({
+                    ...jsonPrueba,
+                    references: event.target.value
+                  });
+                }}
+              />
+            </div>
+
+
           </form>
+
           <button
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            className="btn"
             type="submit"
             onClick={handleSubmit}
             disabled={loading}
