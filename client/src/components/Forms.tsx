2d6e62f3ea31b7316ddc6be8408f9ec08c865233
Debian <aldo@caos.nebftsowrmsudj2uj4exjamv5h.bx.internal.cloudapp.net>
2022-01-09 17:55:58 +0000

    ADDED: Formulario completo en español enviando datos completos al servidor.

--- client/src/components/Forms.tsx
+++ client/src/components/Forms.tsx
@@ -2,49 +2,6 @@ import axios from "axios";
 import { useState } from "react";
 import '../styles/index.css';
 
-type JsonProps = {
-  name: string;
-  image: string;
-  clasification: string;
-  introduction: {
-    etymology: string;
-    atmosphere: string;
-    applications: string;
-    main_locations: string;
-    diffractogram: string;
-  };
-  properties: {
-    chemical: {
-      chemical_formula: string;
-      molecular_weight: string;
-      elemental_chemistry: string;
-      chemistry_oxides: string;
-    },
-    crystallographic:
-    {
-      cell_dimension: string;
-      crystalline_system: string;
-      x_ray_diffraction: string;
-    }
-    ,
-    physical: {
-      gloss: string;
-      color: string;
-      hardness: string;
-      stripe: string;
-      fracture: string;
-      crystal_habit: string;
-      diaphanous: string;
-      exfoliation: string;
-      density: string;
-      luminescence: string;
-      radioactivity: string;
-    },
-    optical: string;
-  };
-  references: string;
-}
-
 // Component that send the form data to the server and returns the response to the user in the form of a JSON object using JsonProps interface and tailwindcss and typescript
 // The form data is sent to the server using axios and the response is returned to the user in the form of a JSON object 
 // to the route "http://localhost:8080/api/rocks/",
@@ -196,47 +153,53 @@ function Forms() {
   // Function that generate the input fields for the form 
   function generateInputs() {
     const inputs = [];
-    const inputNames = [
-      "name",
-      "image",
-      "clasification",
-      "etymology",
-      "atmosphere",
-      "applications",
-      "main_locations",
-      "diffractogram",
-      "chemical_formula",
-      "molecular_weight",
-      "elemental_chemistry",
-      "chemistry_oxides",
-      "cell_dimension",
-      "crystalline_system",
-      "x_ray_diffraction",
-      "gloss",
-      "color",
-      "hardness",
-      "stripe",
-      "fracture",
-      "crystal_habit",
-      "diaphanous",
-      "exfoliation",
-      "density",
-      "luminescence",
-      "radioactivity",
-      "optical",
-      "references",
-    ];
 
-    for (let i = 0; i < inputNames.length; i++) {
+    // Create a dictionary from inputNames and the corresponding state of the component
+    const inputNamesDictionary: { [key: string]: string } = {
+      "name": "Nombre",
+      "image": "Imagen",
+      "clasification": "Clasificación",
+      "etymology": "Etimología",
+      "atmosphere": "Atmósfera",
+      "applications": "Aplicaciones",
+      "main_locations": "Lugares principales",
+      "diffractogram": "Difractograma",
+      "chemical_formula": "Fórmula química",
+      "molecular_weight": "Peso molecular",
+      "elemental_chemistry": "Química elemental",
+      "chemistry_oxides": "Oxígeno",
+      "cell_dimension": "Dimensión de la celda",
+      "crystalline_system": "Sistema cristalino",
+      "x_ray_diffraction": "Difracción por X-ray",
+      "gloss": "Gloss",
+      "color": "Color",
+      "hardness": "Dureza",
+      "stripe": "Rayas",
+      "fracture": "Fractura",
+      "crystal_habit": "Hábito cristalino",
+      "diaphanous": "Diáfano",
+      "exfoliation": "Exfoliación",
+      "density": "Densidad",
+      "luminescence": "Luminosidad",
+      "radioactivity": "Radioactivo",
+      "optical": "Óptico",
+      "references": "Referencias",
+
+    }
+    let llave: string;
+    for (llave in inputNamesDictionary) {
       inputs.push(
-        <div className="mb-4" key={i}>
-          <label className="block text-gray-700 text-sm font-bold mb-2">{inputNames[i]}</label>
+        <div className="form-group" key={llave}>
+          <label 
+          className="block text-gray-700 text-sm font-bold mb-2"
+          htmlFor={llave}>{inputNamesDictionary[llave]}</label>
           <input
             type="text"
-            name={inputNames[i]}
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id={llave}
+            name={llave}
+            value={inputNamesDictionary[llave]}
             onChange={handleChange}
-            value={inputNames[i] === "name" ? name : inputNames[i] === "image" ? image : inputNames[i] === "clasification" ? clasification : inputNames[i] === "etymology" ? etymology : inputNames[i] === "atmosphere" ? atmosphere : inputNames[i] === "applications" ? applications : inputNames[i] === "main_locations" ? main_locations : inputNames[i] === "diffractogram" ? diffractogram : inputNames[i] === "chemical_formula" ? chemical_formula : inputNames[i] === "molecular_weight" ? molecular_weight : inputNames[i] === "elemental_chemistry" ? elemental_chemistry : inputNames[i] === "chemistry_oxides" ? chemistry_oxides : inputNames[i] === "cell_dimension" ? cell_dimension : inputNames[i] === "crystalline_system" ? crystalline_system : inputNames[i] === "x_ray_diffraction" ? x_ray_diffraction : inputNames[i] === "gloss" ? gloss : inputNames[i] === "color" ? color : inputNames[i] === "hardness" ? hardness : inputNames[i] === "stripe" ? stripe : inputNames[i] === "fracture" ? fracture : inputNames[i] === "crystal_habit" ? crystal_habit : inputNames[i] === "diaphanous" ? diaphanous : inputNames[i] === "exfoliation" ? exfoliation : inputNames[i] === "density" ? density : inputNames[i] === "luminescence" ? luminescence : inputNames[i] === "radioactivity" ? radioactivity
-              : inputNames[i] === "optical" ? optical : inputNames[i] === "references" ? references : ""}
           />
         </div>
       );
@@ -251,10 +214,10 @@ function Forms() {
       <div className="flex flex-wrap justify-center">
         <div className="focus-within:border-transparent border-2 border-gray-300 rounded-lg p-4 w-full md:w-1/2">
           <form onSubmit={handleSubmit}>
-          {generateInputs()}
+            {generateInputs()}
           </form>
           <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit" onClick={handleSubmit}>
-            Submit 
+            Submit
           </button>
         </div>
       </div>
